0536ef3b0607a18d06ce52cbfeff35882e110117
Сунагатов Зуфар Рустамович <sunagatovzr@vdcom.ru>
2019-04-24 12:21:20 +0400

    Initial store view with pagination, categories, products.

--- src/app/model/product.repository.ts
+++ src/app/model/product.repository.ts
@@ -10,16 +10,16 @@ export class ProductRepository {
   constructor(private dataSource: StaticDataSource) {
     dataSource.getProducts().subscribe(data => {
       this.products = data;
-      this.categories = data.map(p => p.category).filter((c, index, array) => array.indexOf(c) == index).sort();
+      this.categories = data.map(p => p.category).filter((c, index, array) => array.indexOf(c) === index).sort();
     });
   }
 
   getProducts(category: string = null): Product[] {
-    return this.products.filter(p => category == null || category == p.category);
+    return this.products.filter(p => category == null || category === p.category);
   }
 
   getProduct(id: number): Product {
-    return this.products.find(p => p.id == id);
+    return this.products.find(p => p.id === id);
   }
 
   getCategories(): string[] {
--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -7,14 +7,37 @@ import {ProductRepository} from '../model/product.repository';
   templateUrl: 'store.component.html'
 })
 export class StoreComponent {
+  public selectedCategory = null;
+  public productsPerPage = 4;
+  public selectedPage = 1;
+
   constructor(private repository: ProductRepository) {}
 
   get products(): Product[] {
-    return this.repository.getProducts();
+    const pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+    return this.repository.getProducts(this.selectedCategory).slice(pageIndex, pageIndex + this.productsPerPage);
   }
 
   get categories(): string[] {
     return this.repository.getCategories();
+  }
+
+  changeCategory(newCategory?: string) {
+    this.selectedCategory = newCategory;
+  }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage = Number(newSize);
+    this.changePage(1);
+  }
+
+  get pageNumbers(): number[] {
+    return Array(Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage))
+      .fill(0).map((x, i) => i + 1);
   }
 }
 
